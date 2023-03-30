9f9023ae816933b43fc41ee782cb4962a1c565fd
Datascientistexpert <mbhat.suraj@gmail.com>
2023-03-30 20:53:16 +0200

    fix():..

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from sklearn.preprocessing import StandardScaler
 
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application=Flask(__name__)
+app=Flask(__name__)
 
-app=application
+#app=application
 
 ## Route for a home page
 

4ee8dd94464bb20372a99a4fe097a8da16039779
Datascientistexpert <mbhat.suraj@gmail.com>
2023-03-30 21:24:18 +0200

    !!!

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def predict_datapoint():
         return render_template('home.html',results=math.ceil(results[0]))
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',debug=True)
+    #app.run(host='0.0.0.0',debug=True)
+    app.run(debug=True)
